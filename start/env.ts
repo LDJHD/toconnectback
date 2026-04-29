6a61809cf4862a4f519eeb1f46132f6d41556ca0
jerrytkp <121157081+jerrytkp@users.noreply.github.com>
2026-04-29 13:23:20 +0100

    commit back to test in render app key b

--- start/env.ts
+++ start/env.ts
@@ -1,31 +1,25 @@
-/*
-|--------------------------------------------------------------------------
-| Environment variables service
-|--------------------------------------------------------------------------
-|
-| The `Env.create` method creates an instance of the Env service. The
-| service validates the environment variables and also cast values
-| to JavaScript data types.
-|
-*/
-
-import { Env } from '@adonisjs/core/env'
+import { Env } from '@adonisjs/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
-  APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
 
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring database connection
-  |----------------------------------------------------------
-  */
-  DB_HOST: Env.schema.string({ format: 'host' }),
-  DB_PORT: Env.schema.number(),
-  DB_USER: Env.schema.string(),
+  PORT: Env.schema.number.optional(),
+  HOST: Env.schema.string.optional({ format: 'host' }),
+
+  APP_KEY: Env.schema.string.optional(),
+
+  LOG_LEVEL: Env.schema.enum.optional([
+    'fatal',
+    'error',
+    'warn',
+    'info',
+    'debug',
+    'trace',
+  ] as const),
+
+  DB_HOST: Env.schema.string.optional({ format: 'host' }),
+  DB_PORT: Env.schema.number.optional(),
+  DB_USER: Env.schema.string.optional(),
   DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string()
+  DB_DATABASE: Env.schema.string.optional(),
 })
